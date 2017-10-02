572072a93730a3fdef40c2c25bbbc05be7decb37
antfarmar <markfarrugiamail@gmail.com>
2017-10-02 03:34:43 -0400

    initial commit

---
+++ databases/595 big countries/solution2.sql
@@ -0,0 +1,18 @@
+# Write your MySQL query statement below
+
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    area > 3000000
+
+UNION
+
+SELECT
+    name, population, area
+FROM
+    world
+WHERE
+    population > 25000000
+;
